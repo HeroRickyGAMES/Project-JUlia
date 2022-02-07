65781b3847224775ac77085b51fb27d5464e2871
HeroRickyGAMES <ricojn9@gmail.com>
2022-02-07 14:11:55 -0300

    Adicionado os codigos necessarios para rodar o FIREBASE

--- app/src/main/java/com/herorickystudios/jiulia_socialmidia/MainActivity.java
+++ app/src/main/java/com/herorickystudios/jiulia_socialmidia/MainActivity.java
@@ -2,7 +2,9 @@ package com.herorickystudios.jiulia_socialmidia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void loginacbtn(View view){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
 }
